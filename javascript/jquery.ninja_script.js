beeb60611e5fcc64c4fba395a1147ce3bc089ad8
Judson <nyarly@gmail.com>
2010-12-06 16:55:15 -0800

    Added hack for console.log

--- javascript/jquery.ninja_script.js
+++ javascript/jquery.ninja_script.js
@@ -665,6 +665,13 @@ function buildNinja() {
   return Ninja;  
 };
 
+//More than a bit of a hack
+if(typeof(console) === 'undefined') {
+  console = {
+    log: function(){}
+  }
+}
+
 var Ninja = buildNinja();
 
 //This exists to carry over interfaces from earlier versions of Ninjascript.  Likely, it will be removed from future versions of NinjaScript

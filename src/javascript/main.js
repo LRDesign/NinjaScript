e98779d65a571f1609e55302dc26b0fe080d0095
Judson <nyarly@gmail.com>
2011-09-06 02:26:05 -0700

    Small but vital API change

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,10 +1,24 @@
+window["Ninja"] = {
+  orderList: [],
+  orders: function(order_func){
+    this.orderList.push(order_func)
+  }
+}
 require([
     "ninja",
     "ninja/behaviors/all",
     "ninja/tools/all",
     "ninja/jquery"
   ], function(Ninja, stdBehaviors, placeholder, triggerOn, allTools, jquery) {
+    var ninjaOrders = window["Ninja"].orderList
+    var ordersLength = ninjaOrders.length
+
     window["Ninja"] = Ninja
     Ninja['behavior'] = Ninja.behavior
-
+    for(var i = 0; i < ordersLength; i++) {
+      ninjaOrders[i](Ninja)
+    }
+    Ninja.orders = function(funk) {
+      funk(this) //because it amuses JDL, that's why.
+    }
   })
